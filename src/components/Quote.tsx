f664b02612b6fd07c6eadcc4e57ae010bc78493f
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-23 20:33:19 -0300

    feat: Quote component created

---
+++ src/components/Quote.tsx
@@ -0,0 +1,12 @@
+export function Quote() {
+  return (
+    <div className="border-l-8 border-yellow-200 pl-10">
+      <p className="max-w-[614px] text-4xl font-medium">
+        “The first rule of any technology used in a business is that automation
+        applied to an efficient operation will magnify the efficiency. The
+        second is that automation applied to an inefficient operation will
+        magnify the inefficiency.”
+      </p>
+    </div>
+  )
+}
