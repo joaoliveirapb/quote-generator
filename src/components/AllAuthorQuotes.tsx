c5ed12c62230add02cd78422004b8ae2a4848e26
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-23 23:55:19 -0300

    feat: component to display all author citations

---
+++ src/components/AllAuthorQuotes.tsx
@@ -0,0 +1,31 @@
+import { Quote } from './Quote'
+
+interface QuotesProps {
+  _id: string
+  quoteText: string
+  quoteAuthor: string
+  quoteGenre: string
+}
+
+interface AllAuthorQuotesProps {
+  allAuthorQuotes: QuotesProps[]
+  author: string
+}
+
+export function AllAuthorQuotes({
+  allAuthorQuotes,
+  author,
+}: AllAuthorQuotesProps) {
+  return (
+    <div className="space-y-20">
+      <h1 className="pl-12 text-4xl font-bold text-zinc-700 dark:text-zinc-500">
+        {author}
+      </h1>
+      <div className="space-y-16">
+        {allAuthorQuotes.map((quotes) => (
+          <Quote key={quotes._id} text={quotes.quoteText} />
+        ))}
+      </div>
+    </div>
+  )
+}
