75c9646cec0c9aaa0e34f55a5d639356b62c2d9d
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-23 23:53:52 -0300

    feat: added function to button

--- src/components/AuthorButton.tsx
+++ src/components/AuthorButton.tsx
@@ -3,11 +3,19 @@ import { MoveRight } from 'lucide-react'
 interface AuthorButtonProps {
   author: string
   genre: string
+  getAllAuthorQuotes: (author: string) => void
 }
 
-export function AuthorButton({ author, genre }: AuthorButtonProps) {
+export function AuthorButton({
+  author,
+  genre,
+  getAllAuthorQuotes,
+}: AuthorButtonProps) {
   return (
-    <button className="group mt-10 flex w-full max-w-[600px] items-center justify-between px-6 py-9 transition-colors hover:bg-zinc-800">
+    <button
+      onClick={() => getAllAuthorQuotes(author)}
+      className="group mt-10 flex w-full max-w-[600px] items-center justify-between px-6 py-9 transition-colors hover:bg-zinc-800"
+    >
       <div className="flex flex-col">
         <span className="text-2xl font-bold text-zinc-600 group-hover:text-zinc-100 dark:text-zinc-400">
           {author}
