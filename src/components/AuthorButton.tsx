66312c59005940b6e4fa6ef446fdfa5950945f71
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-23 21:52:38 -0300

    feat: entered property for component

--- src/components/AuthorButton.tsx
+++ src/components/AuthorButton.tsx
@@ -1,14 +1,19 @@
 import { MoveRight } from 'lucide-react'
 
-export function AuthorButton() {
+interface AuthorButtonProps {
+  author: string
+  genre: string
+}
+
+export function AuthorButton({ author, genre }: AuthorButtonProps) {
   return (
     <button className="group mt-10 flex w-full max-w-[600px] items-center justify-between px-6 py-9 transition-colors hover:bg-zinc-800">
       <div className="flex flex-col">
         <span className="text-2xl font-bold text-zinc-600 group-hover:text-zinc-100 dark:text-zinc-400">
-          Bill Gates
+          {author}
         </span>
         <span className="text-left text-sm font-medium text-zinc-500">
-          business
+          {genre}
         </span>
       </div>
       <MoveRight className="invisible text-zinc-100 group-hover:visible" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import { RefreshCw } from 'lucide-react'
 import { ThemeButton } from './ThemeButton'
 
-export function Header() {
+interface HeaderProps {
+  getRandomQuote: () => void
+}
+
+export function Header({ getRandomQuote }: HeaderProps) {
   return (
     <header className="flex justify-end gap-10 px-28 py-4">
-      <button className="flex items-center gap-2 px-2 text-lg font-medium text-zinc-600 transition-colors hover:text-zinc-900 dark:text-zinc-500 dark:hover:text-zinc-400">
+      <button
+        onClick={getRandomQuote}
+        className="flex items-center gap-2 px-2 text-lg font-medium text-zinc-600 transition-colors hover:text-zinc-900 dark:text-zinc-500 dark:hover:text-zinc-400"
+      >
         random
         <RefreshCw size={18} />
       </button>
